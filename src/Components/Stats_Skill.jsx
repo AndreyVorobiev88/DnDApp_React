c6ae485a036f118760a1bacbd95de3f6ce14ba29
AndreyVorobiev88 <Foroven@mail.ru>
2025-03-12 11:21:23 +0300

    modified:   src/Character.json 	new file:   src/Components/StatSkill.css 	new file:   src/Components/Stats_Skill.jsx

---
+++ src/Components/Stats_Skill.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './Stats_Skill.css';
+import '../Main.css';
+
+function Stats_Skill(props) {
+
+
+    return (
+
+        <div></div>
+    )
+
+
+}
+
+export default Stats_Skill
